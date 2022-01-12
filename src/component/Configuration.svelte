b3e84fedee4c3266891cd6ab91dfb2d3521297b7
Trevor Michael <trevormichaelcollins@gmail.com>
2022-01-12 02:25:33 -0500

    Update config page style

--- src/component/Configuration.svelte
+++ src/component/Configuration.svelte
@@ -1,54 +1,13 @@
 <script>
-
+    import ConfigEntityHeader from "./config/ConfigEntityHeader.svelte";
 </script>
 
-<div>
-    <h2>Field Mapping</h2>
-    <div class="row">
-        <div class="col-6">
-            <button id="new-mapping" class="btn btn-primary">New Field Mapping</button>
-            <button id="delete-mappings" class="btn btn-danger">Delete All</button>
-        </div>
-    </div>
-    <div class="row pt-2">
-        <div class="col-12">
-            <ul id="mapping-list" class="list-group">
-            </ul>
-        </div>
-    </div>
-    <br>
-    <h2>Dictionary Entry Mapping</h2>
-    <div class="row">
-        <div class="col-6">
-            <button id="new-entry-mapping" class="btn btn-primary">New Entry Mapping</button>
-            <button id="delete-entry-mappings" class="btn btn-danger">Delete All</button>
-        </div>
-    </div>
-    <div class="row pt-2">
-        <div class="col-12">
-            <ul id="entry-mapping-list" class="list-group">
-            </ul>
-        </div>
-    </div>
-    <br>
-    <h2>Filtering</h2>
-    <div class="row">
-        <div class="col-6">
-            <button id="new-filter" class="btn btn-primary">New Field Filter</button>
-            <button id="delete-filters" class="btn btn-danger">Delete All</button>
-        </div>
-    </div>
-    <div class="row pt-2">
-        <div class="col-12">
-            <ul id="filter-list" class="list-group">
-            </ul>
-        </div>
-    </div>
-    <br>
-    <h2>Export Configuration</h2>
-    <div class="row">
-        <div class="col-6">
-            <button id="export-config" class="btn btn-primary">Export</button>
-        </div>
+<div class="p-3">
+    <ConfigEntityHeader title={"Field Mappings"} buttonText={"New Field Mapping"}/>
+    <ConfigEntityHeader title={"Dictionary Entry Mappings"} buttonText={"New Entry Mapping"}/>
+    <ConfigEntityHeader title={"Note Field Filters"} buttonText={"New Filter"}/>
+    <h6>Export Configuration</h6>
+    <div>
+        <button id="export-config" class="btn btn-primary">Export</button>
     </div>
 </div>
---
+++ src/component/config/ConfigEntityHeader.svelte
@@ -0,0 +1,35 @@
+<script>
+  export let title;
+  export let buttonText;
+</script>
+
+<div class="d-flex mb-3">
+  <h4 class="title me-3">{title}</h4>
+  <button class="btn btn-primary" on:click>
+    <span>
+      <i class="fa-solid fa-plus me-2" />{buttonText}
+    </span>
+  </button>
+  <hr class="flex-grow-1 mx-3">
+</div>
+
+<style>
+  div {
+    justify-items: center;
+  }
+
+  .title {
+    align-self: center;
+    justify-self: center;
+  }
+
+  hr {
+    color: #ffffff90;
+    height: 2px;
+  }
+
+  button {
+    padding: 3px 12px 3px 7px;
+    font-size: 0.9rem;
+  }
+</style>
